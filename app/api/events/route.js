e6e1f56c3f6fc0766b4d647f8534d166877b15cc
MKSTeja <msaiteja.org@hotmail.com>
2026-02-08 18:22:29 +0530

    Update route.js

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -1,30 +1,27 @@
+import data from "@/app/data/sample_events.json";
+
 let cache = {
   data: null,
   lastFetch: 0,
 };
 
-const CACHE_TTL = 60 * 1000; // 1 minute
+const CACHE_TTL = 60 * 1000;
 
 export async function GET() {
   const now = Date.now();
 
-  // Serve cached data if fresh
   if (cache.data && now - cache.lastFetch < CACHE_TTL) {
     return Response.json(cache.data);
   }
 
-  // For now we still use sample_events.json
-  const data = await import("../../data/sample_events.json", {
-    assert: { type: "json" },
-  });
-
   cache = {
-    data: data.default,
+    data,
     lastFetch: now,
   };
 
   return Response.json(cache.data);
 }
+
 
 
 /*
